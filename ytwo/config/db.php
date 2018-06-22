7ef00a508256649b7ca74ff2079d65c3b79c59cb
cy <396557004@qq.com>
2018-06-22 11:05:10 +0800

    2

--- ytwo/config/db.php
+++ ytwo/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ytwo',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
--- ytwo/tests/codeception/config/config.php
+++ ytwo/tests/codeception/config/config.php
@@ -1,17 +1,36 @@
 <?php
+
 /**
  * Application configuration shared by all test types
  */
-return [
-    'components' => [
-        'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
+if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+    return [
+        'components' => [
+            'db' => [
+                'dsn' => 'mysql:host=localhost;dbname=testlinux',
+            ],
+            'mailer' => [
+                'useFileTransport' => true,
+            ],
+            'urlManager' => [
+                'showScriptName' => true,
+            ],
         ],
-        'mailer' => [
-            'useFileTransport' => true,
+    ];
+}else{
+    return [
+        'components' => [
+            'db' => [
+                'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
+            ],
+            'mailer' => [
+                'useFileTransport' => true,
+            ],
+            'urlManager' => [
+                'showScriptName' => true,
+            ],
         ],
-        'urlManager' => [
-            'showScriptName' => true,
-        ],
-    ],
-];
+    ];
+}
+
+
